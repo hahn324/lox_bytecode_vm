6de81f938ded8b1578bc54ca565ffcc74e5f523d
Harrison Hahn <harrisonhahn96@gmail.com>
2025-03-04 18:26:57 -0500

    uncomment stack reset in runtime error and remove Option<Chunk> in Vm

--- src/vm.rs
+++ src/vm.rs
@@ -21,7 +21,7 @@ fn is_falsey(value: Value) -> bool {
 }
 
 pub struct Vm {
-    chunk: Option<Chunk>,
+    chunk: Chunk,
     ip: usize,
     stack: Vec<Value>,
     debug_trace: bool,
@@ -32,7 +32,7 @@ pub struct Vm {
 impl Vm {
     pub fn new(debug_trace: bool) -> Self {
         Self {
-            chunk: None,
+            chunk: Chunk::new(), // Empty chunk placeholder
             ip: 0,
             stack: Vec::with_capacity(256),
             debug_trace,
@@ -45,7 +45,7 @@ impl Vm {
     pub fn interpret(&mut self, source: &str) -> InterpretResult {
         match compile(source, self, self.debug_trace) {
             Some(chunk) => {
-                self.chunk = Some(chunk);
+                self.chunk = chunk;
                 self.ip = 0;
                 self.run()
             }
@@ -70,200 +70,194 @@ impl Vm {
             };
         }
 
-        if let Some(chunk) = self.chunk.take() {
-            loop {
-                if self.debug_trace {
-                    println!("          {:?}", self.stack);
-                    disassemble_instruction(&chunk, self.ip);
+        loop {
+            if self.debug_trace {
+                println!("          {:?}", self.stack);
+                disassemble_instruction(&self.chunk, self.ip);
+            }
+            let instruction = self.chunk.code[self.ip];
+            self.ip += 1;
+            match OpCode::from(instruction) {
+                OpCode::Return => {
+                    // Exit interpreter.
+                    return InterpretResult::InterpretOk;
                 }
-                let instruction = chunk.code[self.ip];
-                self.ip += 1;
-                match OpCode::from(instruction) {
-                    OpCode::Return => {
-                        // Exit interpreter.
-                        return InterpretResult::InterpretOk;
+                OpCode::Constant => {
+                    let offset = self.get_offset();
+                    let constant = self.chunk.constants[offset];
+                    self.stack.push(constant);
+                }
+                OpCode::ConstantLong => {
+                    let offset = self.get_offset_long();
+                    let constant = self.chunk.constants[offset];
+                    self.stack.push(constant);
+                }
+                OpCode::Negate => {
+                    let operand = self
+                        .stack
+                        .pop()
+                        .expect("Stack should be non-empty during NEGATE op.");
+                    match operand {
+                        Value::Number(val) => self.stack.push(Value::Number(-val)),
+                        _ => {
+                            self.runtime_error("Operand must be a number.");
+                            return InterpretResult::InterpretRuntimeError;
+                        }
                     }
-                    OpCode::Constant => {
-                        let offset = self.get_offset(&chunk);
-                        let constant = chunk.constants[offset];
-                        self.stack.push(constant);
+                }
+                OpCode::Add => {
+                    let b = self
+                        .stack
+                        .pop()
+                        .expect("Stack should be non-empty during binary op.");
+                    let a = self
+                        .stack
+                        .pop()
+                        .expect("Stack should be non-empty during binary op.");
+                    match (a, b) {
+                        (Value::String(a_val), Value::String(b_val)) => {
+                            let new_string = format!(
+                                "{}{}",
+                                self.strings.lookup(a_val),
+                                self.strings.lookup(b_val)
+                            );
+                            let str_id = self.strings.intern(new_string.as_str());
+                            self.stack.push(Value::String(str_id));
+                        }
+                        (Value::Number(a_val), Value::Number(b_val)) => {
+                            self.stack.push(Value::Number(a_val + b_val))
+                        }
+                        _ => {
+                            self.runtime_error("Operands must be two numbers or two strings.");
+                            return InterpretResult::InterpretRuntimeError;
+                        }
                     }
-                    OpCode::ConstantLong => {
-                        let offset = self.get_offset_long(&chunk);
-                        let constant = chunk.constants[offset];
-                        self.stack.push(constant);
-                    }
-                    OpCode::Negate => {
-                        let operand = self
-                            .stack
-                            .pop()
-                            .expect("Stack should be non-empty during NEGATE op.");
+                }
+                OpCode::Subtract => binary_op!(Value::Number, -),
+                OpCode::Multiply => binary_op!(Value::Number, *),
+                OpCode::Divide => binary_op!(Value::Number, /),
+                OpCode::Nil => self.stack.push(Value::Nil),
+                OpCode::True => self.stack.push(Value::Bool(true)),
+                OpCode::False => self.stack.push(Value::Bool(false)),
+                OpCode::Not => {
+                    let operand = self
+                        .stack
+                        .pop()
+                        .expect("Stack should be non-empty during NOT op.");
+                    self.stack.push(Value::Bool(is_falsey(operand)));
+                }
+                OpCode::Equal => {
+                    let a = self.stack.pop().unwrap();
+                    let b = self.stack.pop().unwrap();
+                    self.stack.push(Value::Bool(a.eq(&b)));
+                }
+                OpCode::Greater => binary_op!(Value::Bool, >),
+                OpCode::Less => binary_op!(Value::Bool, <),
+                OpCode::Print => {
+                    if let Some(operand) = self.stack.pop() {
                         match operand {
-                            Value::Number(val) => self.stack.push(Value::Number(-val)),
-                            _ => {
-                                self.runtime_error("Operand must be a number.");
-                                return InterpretResult::InterpretRuntimeError;
+                            Value::Number(val) => println!("{val}"),
+                            Value::Bool(val) => println!("{val}"),
+                            Value::String(str_id) => {
+                                println!("{}", self.strings.lookup(str_id));
                             }
+                            Value::Nil => println!("Nil"),
+                            Value::Undefined => println!("Undefined"),
                         }
                     }
-                    OpCode::Add => {
-                        let b = self
-                            .stack
-                            .pop()
-                            .expect("Stack should be non-empty during binary op.");
-                        let a = self
-                            .stack
-                            .pop()
-                            .expect("Stack should be non-empty during binary op.");
-                        match (a, b) {
-                            (Value::String(a_val), Value::String(b_val)) => {
-                                let new_string = format!(
-                                    "{}{}",
-                                    self.strings.lookup(a_val),
-                                    self.strings.lookup(b_val)
-                                );
-                                let str_id = self.strings.intern(new_string.as_str());
-                                self.stack.push(Value::String(str_id));
-                            }
-                            (Value::Number(a_val), Value::Number(b_val)) => {
-                                self.stack.push(Value::Number(a_val + b_val))
-                            }
-                            _ => {
-                                self.runtime_error("Operands must be two numbers or two strings.");
-                                return InterpretResult::InterpretRuntimeError;
-                            }
+                }
+                OpCode::Pop => {
+                    self.stack.pop();
+                }
+                OpCode::DefineGlobal => {
+                    let offset = self.get_offset();
+                    self.global_values[offset] = self.stack.pop().expect(
+                        "Stack pop will always return Some variant when defining a global.",
+                    );
+                }
+                OpCode::DefineGlobalLong => {
+                    let offset = self.get_offset_long();
+                    self.global_values[offset] = self.stack.pop().expect(
+                        "Stack pop will always return Some variant when defining a global.",
+                    );
+                }
+                OpCode::GetGlobal => {
+                    let offset = self.get_offset();
+                    let value = self.global_values[offset];
+                    match value {
+                        Value::Undefined => {
+                            self.undefined_global_variable(offset);
+                            return InterpretResult::InterpretRuntimeError;
                         }
+                        _ => (),
                     }
-                    OpCode::Subtract => binary_op!(Value::Number, -),
-                    OpCode::Multiply => binary_op!(Value::Number, *),
-                    OpCode::Divide => binary_op!(Value::Number, /),
-                    OpCode::Nil => self.stack.push(Value::Nil),
-                    OpCode::True => self.stack.push(Value::Bool(true)),
-                    OpCode::False => self.stack.push(Value::Bool(false)),
-                    OpCode::Not => {
-                        let operand = self
-                            .stack
-                            .pop()
-                            .expect("Stack should be non-empty during NOT op.");
-                        self.stack.push(Value::Bool(is_falsey(operand)));
-                    }
-                    OpCode::Equal => {
-                        let a = self.stack.pop().unwrap();
-                        let b = self.stack.pop().unwrap();
-                        self.stack.push(Value::Bool(a.eq(&b)));
-                    }
-                    OpCode::Greater => binary_op!(Value::Bool, >),
-                    OpCode::Less => binary_op!(Value::Bool, <),
-                    OpCode::Print => {
-                        if let Some(operand) = self.stack.pop() {
-                            match operand {
-                                Value::Number(val) => println!("{val}"),
-                                Value::Bool(val) => println!("{val}"),
-                                Value::String(str_id) => {
-                                    println!("{}", self.strings.lookup(str_id));
-                                }
-                                Value::Nil => println!("Nil"),
-                                Value::Undefined => println!("Undefined"),
-                            }
+                    self.stack.push(value);
+                }
+                OpCode::GetGlobalLong => {
+                    let offset = self.get_offset_long();
+                    let value = self.global_values[offset];
+                    match value {
+                        Value::Undefined => {
+                            self.undefined_global_variable(offset);
+                            return InterpretResult::InterpretRuntimeError;
                         }
+                        _ => (),
                     }
-                    OpCode::Pop => {
-                        self.stack.pop();
-                    }
-                    OpCode::DefineGlobal => {
-                        let offset = self.get_offset(&chunk);
-                        self.global_values[offset] = self.stack.pop().expect(
-                            "Stack pop will always return Some variant when defining a global.",
-                        );
-                    }
-                    OpCode::DefineGlobalLong => {
-                        let offset = self.get_offset_long(&chunk);
-                        self.global_values[offset] = self.stack.pop().expect(
-                            "Stack pop will always return Some variant when defining a global.",
-                        );
-                    }
-                    OpCode::GetGlobal => {
-                        let offset = self.get_offset(&chunk);
-                        let value = self.global_values[offset];
-                        match value {
-                            Value::Undefined => {
-                                self.undefined_global_variable(offset);
-                                return InterpretResult::InterpretRuntimeError;
-                            }
-                            _ => (),
+                    self.stack.push(value);
+                }
+                OpCode::SetGlobal => {
+                    let offset = self.get_offset();
+                    match self.global_values[offset] {
+                        Value::Undefined => {
+                            self.undefined_global_variable(offset);
+                            return InterpretResult::InterpretRuntimeError;
                         }
-                        self.stack.push(value);
+                        _ => (),
                     }
-                    OpCode::GetGlobalLong => {
-                        let offset = self.get_offset_long(&chunk);
-                        let value = self.global_values[offset];
-                        match value {
-                            Value::Undefined => {
-                                self.undefined_global_variable(offset);
-                                return InterpretResult::InterpretRuntimeError;
-                            }
-                            _ => (),
+                    self.global_values[offset] = self.stack[self.stack.len() - 1];
+                }
+                OpCode::SetGlobalLong => {
+                    let offset = self.get_offset_long();
+                    match self.global_values[offset] {
+                        Value::Undefined => {
+                            self.undefined_global_variable(offset);
+                            return InterpretResult::InterpretRuntimeError;
                         }
-                        self.stack.push(value);
+                        _ => (),
                     }
-                    OpCode::SetGlobal => {
-                        let offset = self.get_offset(&chunk);
-                        match self.global_values[offset] {
-                            Value::Undefined => {
-                                self.undefined_global_variable(offset);
-                                // NOTE: Value on stack will not get popped since returning before
-                                // OpCode::Pop op for expression statement... Memory leak in REPL?
-                                return InterpretResult::InterpretRuntimeError;
-                            }
-                            _ => (),
-                        }
-                        self.global_values[offset] = self.stack[self.stack.len() - 1];
-                    }
-                    OpCode::SetGlobalLong => {
-                        let offset = self.get_offset_long(&chunk);
-                        match self.global_values[offset] {
-                            Value::Undefined => {
-                                self.undefined_global_variable(offset);
-                                // NOTE: Value on stack will not get popped since returning before
-                                // OpCode::Pop op for expression statement... Memory leak in REPL?
-                                return InterpretResult::InterpretRuntimeError;
-                            }
-                            _ => (),
-                        }
-                        self.global_values[offset] = self.stack[self.stack.len() - 1];
-                    }
+                    self.global_values[offset] = self.stack[self.stack.len() - 1];
                 }
             }
-        } else {
-            return InterpretResult::InterpretRuntimeError;
         }
     }
 
-    fn get_offset(&mut self, chunk: &Chunk) -> usize {
-        let offset = chunk.code[self.ip] as usize;
+    fn get_offset(&mut self) -> usize {
+        let offset = self.chunk.code[self.ip] as usize;
         self.ip += 1;
         offset
     }
 
-    fn get_offset_long(&mut self, chunk: &Chunk) -> usize {
-        let right_byte = chunk.code[self.ip] as usize;
-        let middle_byte = chunk.code[self.ip + 1] as usize;
-        let left_byte = chunk.code[self.ip + 2] as usize;
+    fn get_offset_long(&mut self) -> usize {
+        let right_byte = self.chunk.code[self.ip] as usize;
+        let middle_byte = self.chunk.code[self.ip + 1] as usize;
+        let left_byte = self.chunk.code[self.ip + 2] as usize;
         let offset = (right_byte << 16) + (middle_byte << 8) + left_byte;
         self.ip += 3;
         offset
     }
 
-    fn runtime_error(&self, err_msg: &str) {
-        eprintln!("{err_msg}");
-        if let Some(ref chunk) = self.chunk {
-            let line = chunk.get_line(self.ip - 1);
-            eprintln!("[line {line}] in script.");
-        }
-        // self.reset_stack();
+    fn runtime_error(&mut self, err_msg: &str) {
+        eprintln!("Runtime Error: {err_msg}");
+        let line = self.chunk.get_line(self.ip - 1);
+        eprintln!("[line {line}] in script.");
+        self.reset_stack();
     }
 
-    fn undefined_global_variable(&self, offset: usize) {
+    fn reset_stack(&mut self) {
+        self.stack.clear();
+    }
+
+    fn undefined_global_variable(&mut self, offset: usize) {
         for (str_id, val_offset) in self.global_names.iter() {
             if offset == *val_offset {
                 let var_name = self.strings.lookup(*str_id);
